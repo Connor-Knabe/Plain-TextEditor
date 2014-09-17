ad1050be9a46b98e94e0e6d88e69f144074d2e28
Connor Knabe <connorknabe@gmail.com>
2014-09-17 13:23:46 -0500

    Added finished project to git

---
+++ PlainTextEditor-ConnorKnabe/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace PlainTextEditor_ConnorKnabe {   
+    public partial class MainWindow : Window {
+        TextDocument textDocument = new TextDocument();
+
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e) {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                if (!textDocument.saveFile(saveFileDialog.FileName, txtInput.Text)) {
+                    // http://msdn.microsoft.com/en-us/library/Aa984357
+                    System.Windows.Forms.MessageBox.Show("An error occurred saving the report.", "Grader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            // http://msdn.microsoft.com/en-us/library/db5x7c0d(v=VS.85).aspx
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                textDocument.filePath = openFileDialog.FileName;
+                if (textDocument.openFile()) {
+                    txtInput.Text = textDocument.textBoxString;
+
+                }
+            }
+        }
+
+       
+
+      
+    }
+}
---
+++ PlainTextEditor-ConnorKnabe/TextDocument.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace PlainTextEditor_ConnorKnabe {
+    class TextDocument {
+
+        public String filePath;
+        //public List<String> textBoxString = new List<String>();
+        public String textBoxString;
+        public Boolean saveFile(String fileName, String text) {
+            try {
+                using (StreamWriter sw = new StreamWriter(fileName)) {
+                    sw.WriteLine(text);
+                }
+                return true;
+            } catch (Exception) {
+                return false;
+            }
+        }
+
+        public Boolean openFile() {
+
+            try {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(filePath)) {
+                    // http://msdn.microsoft.com/en-us/library/ms228388(v=vs.80).aspx
+                    // http://msdn.microsoft.com/en-us/library/ms131448.aspx
+                    String line = "";
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null) {
+                        textBoxString=line;
+
+                    }
+                }
+
+                return true;
+
+            } catch (Exception ex) {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
